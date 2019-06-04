2f0a23e83dd395097d6bb126b59e8ec76b562b1f
Jerrico Gamis <jecklgamis@gmail.com>
2019-06-05 07:47:36 +1000

    Use Java time module

--- src/main/kotlin/springboot/kotlin/example/ObjectMapperBuilderCustomizer.kt
+++ src/main/kotlin/springboot/kotlin/example/ObjectMapperBuilderCustomizer.kt
@@ -1,7 +1,6 @@
 package springboot.kotlin.example
 
-import com.fasterxml.jackson.databind.SerializationFeature
-import com.fasterxml.jackson.datatype.joda.JodaModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 import org.springframework.stereotype.Component
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Component
 @Component
 class ObjectMapperBuilderCustomizer : Jackson2ObjectMapperBuilderCustomizer {
     override fun customize(builder: Jackson2ObjectMapperBuilder) {
-        builder.modules(JodaModule())
-        builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        builder.modules(JavaTimeModule())
     }
 }
--- src/main/kotlin/springboot/kotlin/example/controller/RootController.kt
+++ src/main/kotlin/springboot/kotlin/example/controller/RootController.kt
@@ -2,10 +2,10 @@ package springboot.kotlin.example.controller
 
 import com.google.common.collect.ImmutableMap
 import io.micrometer.core.annotation.Timed
-import org.joda.time.LocalDateTime
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping(path = ["/"])
