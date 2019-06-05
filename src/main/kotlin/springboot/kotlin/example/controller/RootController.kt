8639aacedbb61831255149782bbb21afeb822ee3
Jerrico Gamis <jecklgamis@gmail.com>
2019-06-05 20:02:28 +1000

    Update root controller

--- src/main/kotlin/springboot/kotlin/example/controller/RootController.kt
+++ src/main/kotlin/springboot/kotlin/example/controller/RootController.kt
@@ -2,21 +2,24 @@ package springboot.kotlin.example.controller
 
 import com.google.common.collect.ImmutableMap
 import io.micrometer.core.annotation.Timed
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod.GET
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDateTime
 
 @RestController
-@RequestMapping(path = ["/"])
 class RootController {
+    @Value("\${application.name}")
+    private val appName: String? = null
 
     @RequestMapping(path = ["/"], method = [GET], produces = ["application/json"])
     @Timed
-    fun example(): Map<String, Any> {
+    fun root(): Map<String, Any> {
         return ImmutableMap.builder<String, Any>()
-                .put("message", "It works!")
-                .put("today", LocalDateTime.now())
-                .build();
+                .put("name", appName)
+                .put("java.version", System.getProperty("java.version"))
+                .put("now", LocalDateTime.now())
+                .build()
     }
 }
